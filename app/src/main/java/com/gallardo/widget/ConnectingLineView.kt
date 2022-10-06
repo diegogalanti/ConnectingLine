d8ff967fa4fece21b849ca019af5621882f7bb69
diegogalanti <dggalanti@hotmail.com>
2022-10-06 11:39:15 +0200

    Included preferred path, where it is possible to select side-to-side or top-to-bottom path. Also checks if the preferred path is possible or not depending on the origin and destination view locations.

--- app/src/main/java/com/gallardo/widget/ConnectingLineView.kt
+++ app/src/main/java/com/gallardo/widget/ConnectingLineView.kt
@@ -1,17 +1,12 @@
 package com.gallardo.widget
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
-import android.util.Log
-import android.util.TypedValue
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import java.lang.Math.abs
-
 
 class ConnectingLineView(
     context: Context,
@@ -40,10 +35,17 @@ class ConnectingLineView(
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.STROKE
-        strokeWidth = 2F
+        color = Color.RED
+        strokeWidth = 4F
     }
 
     private val set = ConstraintSet()
+
+    private var dentSize = 20.0F
+
+    private var radiusCornerEffect = 5.0F
+
+    var preferredPath = SIDE_TO_SIDE
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
@@ -54,6 +56,7 @@ class ConnectingLineView(
 
         adjustConstraintSet(originView, destinationView)
         set.applyTo(constraintLayout)
+        paint.pathEffect = CornerPathEffect(radiusCornerEffect)
         canvas.drawPath(createPath(originView, destinationView), paint)
     }
 
@@ -89,62 +92,168 @@ class ConnectingLineView(
     }
 
     private fun createPath(originView: View, destinationView: View): Path {
+        return when(preferredPath){
+            SIDE_TO_SIDE -> {
+                if (isSideToSidePossible(originView, destinationView))
+                    createPathSideToSide(originView, destinationView)
+                else if (isTopToBottomPossible(originView, destinationView))
+                    createPathTopToBottom(originView, destinationView)
+                else
+                    Path()
+            }
+            else -> {
+                if (isTopToBottomPossible(originView, destinationView))
+                    createPathTopToBottom(originView, destinationView)
+                else if (isSideToSidePossible(originView, destinationView))
+                    createPathSideToSide(originView, destinationView)
+                else
+                    Path()
+            }
+        }
+    }
+
+    private fun createPathSideToSide(originView: View, destinationView: View): Path {
         val optimalPath = Path()
         val distLeftRight = abs(originView.left - destinationView.right)
         val distRightLeft = abs(originView.right - destinationView.left)
-        val distTopBottom = abs(originView.top - destinationView.bottom)
-        val distBottomTop = abs(originView.bottom - destinationView.top)
+        val verticalDist = abs((originView.top + (originView.height / 2)) - (destinationView.top + (destinationView.height / 2))).toFloat()
 
         val arrHorizontalDist = listOf(distLeftRight, distRightLeft)
+
+        if(isAbove(originView, destinationView))
+            when (arrHorizontalDist.min()) {
+                distLeftRight -> {
+                    optimalPath.moveTo(
+                        destinationView.width.toFloat() + distLeftRight,
+                        originView.height.toFloat() / 2
+                    )
+                    optimalPath.rLineTo(-dentSize, 0.0F)
+                    optimalPath.rLineTo(0.0F, verticalDist)
+                    optimalPath.lineTo(
+                        destinationView.width.toFloat(),
+                        originView.height.toFloat() / 2 + verticalDist
+                    )
+                }
+                distRightLeft -> {
+                    optimalPath.moveTo(
+                        originView.width.toFloat(),
+                        originView.height.toFloat() / 2)
+                    optimalPath.rLineTo(dentSize, 0.0F)
+                    optimalPath.rLineTo(0.0F, verticalDist)
+                    optimalPath.lineTo(
+                        originView.width.toFloat() + distRightLeft.toFloat(),
+                        originView.height.toFloat() / 2 + verticalDist
+                    )
+                }
+            }
+        else
+            when (arrHorizontalDist.min()) {
+                distLeftRight -> {
+                    optimalPath.moveTo(
+                        destinationView.width.toFloat() + distLeftRight,
+                        destinationView.height.toFloat() - destinationView.height / 2 + verticalDist//qualquer coisa ajustar aqui segundo para origin
+                    )
+                    optimalPath.rLineTo(-dentSize, 0.0F)
+                    optimalPath.rLineTo(0.0F, -verticalDist)
+                    optimalPath.lineTo(
+                        destinationView.width.toFloat(),
+                        destinationView.height.toFloat() / 2
+                    )
+                }
+                distRightLeft -> {
+                    optimalPath.moveTo(
+                        originView.width.toFloat(),
+                        destinationView.height.toFloat() - destinationView.height / 2 + verticalDist //qualquer coisa ajustar aqui segundo para origin
+                    )
+                    optimalPath.rLineTo(dentSize, 0.0F)
+                    optimalPath.rLineTo(0.0F, -verticalDist)
+                    optimalPath.lineTo(
+                        originView.width.toFloat() + distRightLeft,
+                        destinationView.height.toFloat() / 2
+                    )
+                }
+            }
+        return optimalPath
+    }
+
+    private fun createPathTopToBottom(originView: View, destinationView: View): Path {
+        val optimalPath = Path()
+        val distTopBottom = abs(originView.top - destinationView.bottom)
+        val distBottomTop = abs(originView.bottom - destinationView.top)
+        val horizontalDist = abs((originView.left + (originView.width / 2)) - (destinationView.left + (destinationView.width / 2))).toFloat()
+
         val arrVerticalDist = listOf(distTopBottom, distBottomTop)
 
-        when (arrVerticalDist.min()) {
-            distBottomTop ->
-                when (arrHorizontalDist.min()) {
-                    distLeftRight -> {
-                        optimalPath.moveTo(
-                            destinationView.width.toFloat() + distLeftRight,
-                            originView.height.toFloat()
-                        )
-                        optimalPath.lineTo(
-                            destinationView.width.toFloat(),
-                            originView.height.toFloat() + distBottomTop
-                        )
-                    }
-                    distRightLeft -> {
-                        optimalPath.moveTo(
-                            originView.width.toFloat(),
-                            originView.height.toFloat())
-                        optimalPath.lineTo(
-                            originView.width.toFloat() + distRightLeft.toFloat(),
-                            originView.height.toFloat() + distBottomTop
-                        )
-                    }
+        if(isLeft(originView, destinationView))
+            when (arrVerticalDist.min()) {
+                distTopBottom -> {
+                    optimalPath.moveTo(
+                        originView.width.toFloat() / 2,
+                        destinationView.height.toFloat() + distTopBottom
+                    )
+                    optimalPath.rLineTo(0.0F, -dentSize)
+                    optimalPath.rLineTo(horizontalDist, 0.0F)
+                    optimalPath.lineTo(
+                        originView.width.toFloat() / 2 + horizontalDist,
+                        destinationView.height.toFloat()
+                    )
                 }
-            distTopBottom ->
-                when (arrHorizontalDist.min()) {
-                    distLeftRight -> {
-                        optimalPath.moveTo(
-                            destinationView.width.toFloat() + distLeftRight,
-                            destinationView.height.toFloat() + distTopBottom
-                        )
-                        optimalPath.lineTo(
-                            destinationView.width.toFloat(),
-                            destinationView.height.toFloat()
-                        )
-                    }
-                    distRightLeft -> {
-                        optimalPath.moveTo(
-                            originView.width.toFloat(),
-                            destinationView.height.toFloat() + distTopBottom
-                        )
-                        optimalPath.lineTo(
-                            originView.width.toFloat() + distRightLeft,
-                            destinationView.height.toFloat()
-                        )
-                    }
+                distBottomTop -> {
+                    optimalPath.moveTo(
+                        originView.width.toFloat() / 2,
+                        originView.height.toFloat())
+                    optimalPath.rLineTo(0.0F, dentSize)
+                    optimalPath.rLineTo(horizontalDist, 0.0F)
+                    optimalPath.lineTo(
+                        originView.width.toFloat() / 2 + horizontalDist,
+                        originView.height.toFloat() + distBottomTop.toFloat()
+                    )
                 }
-        }
+            }
+        else
+            when (arrVerticalDist.min()) {
+                distTopBottom -> {
+                    optimalPath.moveTo(
+                        destinationView.width.toFloat() - destinationView.width / 2 + horizontalDist,
+                        destinationView.height.toFloat() + distTopBottom
+                    )
+                    optimalPath.rLineTo(0.0F, -dentSize)
+                    optimalPath.rLineTo(-horizontalDist, 0.0F)
+                    optimalPath.lineTo(
+                        destinationView.width.toFloat() / 2,
+                        destinationView.height.toFloat()
+                    )
+                }
+                distBottomTop -> {
+                    optimalPath.moveTo(
+                        destinationView.width.toFloat() - destinationView.width / 2 + horizontalDist,
+                        originView.height.toFloat()
+                    )
+                    optimalPath.rLineTo(0.0F, dentSize)
+                    optimalPath.rLineTo(-horizontalDist, 0.0F)
+                    optimalPath.lineTo(
+                        destinationView.width.toFloat() / 2,
+                        originView.height.toFloat() + distBottomTop
+                    )
+                }
+            }
         return optimalPath
+    }
+
+    private fun isSideToSidePossible(originView : View, destinationView : View): Boolean {
+        return originView.right < destinationView.left || originView.left > destinationView.right
+    }
+
+    private fun isTopToBottomPossible(originView : View, destinationView : View): Boolean {
+        return originView.bottom < destinationView.top || originView.top > destinationView.bottom
+    }
+
+    private fun isAbove(originView : View, destinationView : View) = (originView.top + (originView.height / 2)) - (destinationView.top + (destinationView.height / 2)) < 0
+
+    private fun isLeft(originView : View, destinationView : View) = (originView.left + (originView.width / 2)) - (destinationView.left + (destinationView.width / 2)) < 0
+
+    companion object PathDirection{
+        val SIDE_TO_SIDE = 0;
+        val TOP_TO_BOTTOM = 1;
     }
 }
