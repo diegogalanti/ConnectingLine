b61ff99e9afcd8454050f0d789bc5a14187414dd
diegogalanti <dggalanti@hotmail.com>
2022-10-13 20:39:46 +0200

    Implemented moving View to increase testing speed.

--- app/src/main/java/com/gallardo/widget/MainActivity.kt
+++ app/src/main/java/com/gallardo/widget/MainActivity.kt
@@ -6,13 +6,16 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.MenuItem
+import android.view.MotionEvent
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.annotation.MenuRes
 import androidx.appcompat.widget.PopupMenu
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.view.marginLeft
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
@@ -42,36 +45,62 @@ class MainActivity : AppCompatActivity() {
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 val connectingLine1 = findViewById<ConnectingLineView>(R.id.five_to_one)
-                val connectingLine2 = findViewById<ConnectingLineView>(R.id.five_to_two)
-                val connectingLine3 = findViewById<ConnectingLineView>(R.id.five_to_three)
-                val connectingLine4 = findViewById<ConnectingLineView>(R.id.five_to_four)
-                val connectingLine6 = findViewById<ConnectingLineView>(R.id.five_to_six)
-                val connectingLine7 = findViewById<ConnectingLineView>(R.id.five_to_seven)
-                val connectingLine8 = findViewById<ConnectingLineView>(R.id.five_to_eight)
-                val connectingLine9 = findViewById<ConnectingLineView>(R.id.five_to_nine)
+//                val connectingLine2 = findViewById<ConnectingLineView>(R.id.five_to_two)
+//                val connectingLine3 = findViewById<ConnectingLineView>(R.id.five_to_three)
+//                val connectingLine4 = findViewById<ConnectingLineView>(R.id.five_to_four)
+//                val connectingLine6 = findViewById<ConnectingLineView>(R.id.five_to_six)
+//                val connectingLine7 = findViewById<ConnectingLineView>(R.id.five_to_seven)
+//                val connectingLine8 = findViewById<ConnectingLineView>(R.id.five_to_eight)
+//                val connectingLine9 = findViewById<ConnectingLineView>(R.id.five_to_nine)
 
                 for (i in 0..17) {
                     if(s.toString() == items[i]) {
                         connectingLine1.preferredPath = i
-                        connectingLine2.preferredPath = i
-                        connectingLine3.preferredPath = i
-                        connectingLine4.preferredPath = i
-                        connectingLine6.preferredPath = i
-                        connectingLine7.preferredPath = i
-                        connectingLine8.preferredPath = i
-                        connectingLine9.preferredPath = i
+//                        connectingLine2.preferredPath = i
+//                        connectingLine3.preferredPath = i
+//                        connectingLine4.preferredPath = i
+//                        connectingLine6.preferredPath = i
+//                        connectingLine7.preferredPath = i
+//                        connectingLine8.preferredPath = i
+//                        connectingLine9.preferredPath = i
                         connectingLine1.invalidate()
-                        connectingLine2.invalidate()
-                        connectingLine3.invalidate()
-                        connectingLine4.invalidate()
-                        connectingLine6.invalidate()
-                        connectingLine7.invalidate()
-                        connectingLine8.invalidate()
-                        connectingLine9.invalidate()
+//                        connectingLine2.invalidate()
+//                        connectingLine3.invalidate()
+//                        connectingLine4.invalidate()
+//                        connectingLine6.invalidate()
+//                        connectingLine7.invalidate()
+//                        connectingLine8.invalidate()
+//                        connectingLine9.invalidate()
                     }
                 }
                 findViewById<ConstraintLayout>(R.id.cl).invalidate()
             }
         })
+        val dest = findViewById<View>(R.id.one)
+        dest.setOnTouchListener { v, event ->
+            var rightDX : Float = 0f
+            var rightDY : Float = 0f
+            when(event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    rightDX = v.x - event.rawX
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    val constraint1 = ConstraintSet()
+                    constraint1.clone(findViewById<ConstraintLayout>(R.id.cl))
+                    constraint1.setMargin(R.id.one, ConstraintLayout.LayoutParams.START,
+                        event.rawX.toInt() - 65
+                    )
+                    constraint1.setMargin(R.id.one, ConstraintLayout.LayoutParams.TOP,
+                        event.rawY.toInt() - 340
+                    )
+                    constraint1.applyTo(findViewById(R.id.cl))
+                }
+                else -> {
+                    return@setOnTouchListener false
+                }
+            }
+            v.performClick()
+            true
+        }
     }
 }
